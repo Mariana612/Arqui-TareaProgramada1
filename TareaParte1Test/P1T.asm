cc65f009822c4b7310c38d826984ce76d2f9cad9
Val0217 <vduran0217@gmail.com>
2024-05-17 10:43:03 -0600

    no sirve

--- TareaParte1Test/P1T.asm
+++ TareaParte1Test/P1T.asm
@@ -1,6 +1,7 @@
 section .data
     espacio db 10             ; New line character for syscalls
     text_start db 'hola me llamo sandia carrasco y me gusta las sandia amarrilla no la roja es que la roja es fea, ademas la gente solo piensa en la sandia roja esto es una palabra por favor me gusta holi.', 0
+	digits db '0123456789ABCDEF' 
 	; --Menu
 	text_menuInicial db 'Bienvenido a el editor de documnetos, que desea realizar?', 0
 	text_menuInicialCont db 'a) Editar un documento b) Terminar Programa', 0
@@ -11,7 +12,10 @@ section .data
 	lentext dq 0 
 	lenUserText dq 0
 	lenStartingText dq 0
+    binary_str db '1011', 0
+    decimal_number dd 0
 
+	
 	
 
 section .bss
@@ -26,276 +30,82 @@ section .bss
     new_text resb 2050 ; Buffer para el texto final.
     
     buffer resb 2050
+    
+    result resb 2050
+    hex_result resb 16
 
 section .text
     global _start
+    
 
 _start:
-	mov rax, text_menuInicial
-    call _genericprint  
-    call _enterPrint 
+	;mov rax, text_menuInicial
+    ;call _genericprint  
+    ;call _enterPrint 
     
-    mov rax, text_menuInicialCont
-    call _genericprint   
-    call _enterPrint
+    ;mov rax, text_menuInicialCont
+    ;call _genericprint   
+    ;call _enterPrint
     
     
-    call get_input
-    call menu_compare
+    ;call get_input
+    ;call menu_compare
+    
+    mov rdi, binary_str
+    call _calculate_size
+    mov rsi, [lentext]
+    call is_binary
+    
+    mov rsi, rax
+    call _startItoa
+    
+    mov rax, buffer
+    call _genericprint
+	
+   call ascii_to_hex
+   
+   mov rax, hex_result
+   call _genericprint
+    
+    
     
     jmp _finishCode
 
 ;-------------- INTERACCION CON EL USUARIO -----------------------
-get_input:
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, user_input
-    mov rdx, 2050
-    syscall
-    ret
-
-menu_compare:   
-    cmp byte[user_input], 'a'		; Up, se mueve una línea de texto hacia arriba
-    je _manageEdit
-    cmp byte[user_input], 'b'		; Quit, se sale del programa
-    je _finishCode
-    ret
 
 ;-------------- INTERACCION CON EL USUARIO -----------------------  
-    
-;-------------- PRINT DE TODO EL DOCUMENTO CON SUS LINEAS --------------
-_startFullPrint:
-    mov [lastPrint], r9       ; Initialize lastPrint to start of text
-    
-    
 
-_fullPrint:
-    xor rax, rax              ; Clear rax
-    mov qword [lineCount], 0    	; Inicializar contador de líneas
-    mov qword [printCont], 0        ; Inicializar contador de caracteres por linea
-    mov qword [wordCount], 0
-    mov qword[lenFirstPart], -1
+;---------------COMPARE BINARIO-----------------------------------
 
-_printLoopFP:
-    mov cl, [r9]
-    test cl, cl
-    jz _endPrintFP              ; End if NULL character is reached
-    inc qword[lenFirstPart]
+is_binary:
+    xor rcx, rcx      
 
-    cmp cl, ' '
-    je _checkWordBoundaryFP
-    cmp cl, 9                 ; ASCII for Tab
-    je _checkWordBoundaryFP
-    cmp cl, 10                ; ASCII for Newline
-    je _checkWordBoundaryFP
+.loop:
+    cmp rcx, rsi
+    jge .done             
 
-    inc qword[printCont]           ; Increment character counter
-    inc r9                    ; Move to next character
-    jmp _printLoopFP
+    mov al, [rdi + rcx]
 
-_checkWordBoundaryFP:
-    inc qword[wordCount]           ; Increment word count
-    cmp qword[wordCount], 10
-    jl _incrementCharFP         ; If less than 10 words, keep printing
-    
-    mov rsi, [lineCount]
-    call _startItoa
-    
-    cmp byte[flag_printOnePhrase], 0
-    je _firstcontinueBoundary
+    cmp al, '0'
+    je .next          
 
+    cmp al, '1'
+    je .next      
 
-    mov rax, [buffer] ; Load '1' from userInput into AL
-    mov rbx, [user_input]    ; Load '1' from buffer into BL
-
-    cmp rax, rbx
-    jne _skipLinePLTE
-    
-    _firstcontinueBoundary:
     mov rax, 1
-    mov rdi, 1
-    mov rsi, buffer
-    mov rdx, 5              ; Number of characters from lastPrint to current position
-    syscall
-
-
-	mov rax, 1                  ; syscall: write
-	mov rdi, 1                  ; fd: stdout
-	mov rsi, [lastPrint]        ; Start of string to print
-	mov rdx, r9
-	sub rdx, [lastPrint]        ; Calculate length to print
-	test rdx, rdx               ; Check if length is positive
-	jle _endPrintFP              ; Skip printing if length is not positive
-	syscall                     ; Perform print
-	
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, espacio          ; Print newline
-    mov rdx, 1
-    syscall
-    
-    cmp byte[flag_printOnePhrase], 1
-    je _finishSpecialFP
-    
-    jmp _continueBoundary
-    
-    _skipLinePLTE:				; CHEQUEO por si no se necesita el print
-
-	mov rdx, r9
-	sub rdx, [lastPrint]        ; Calculate length to print
-	test rdx, rdx               ; Check if length is positive
-	jle _endPrintFP              ; Skip printing if length is not positive
-	syscall 
-
-	_continueBoundary:
-    mov [lastPrint], r9       ; Update lastPrint to new position
-    xor rax, rax
-    mov [wordCount], rax      ; Reset word count
-    inc qword[lineCount]           ; Increment line count
-
-_incrementCharFP:
-    inc qword[printCont]           ; Increment character counter
-    inc r9                    ; Move to next character
-    jmp _printLoopFP
-
-_endPrintFP:
-    mov rsi, [lineCount]
-    call _startItoa
-    
-    cmp byte[flag_printOnePhrase], 0
-    je _firstcontinueEP
-
-    mov rax, [buffer] ; Load '1' from userInput into AL
-    mov rbx, [user_input]    ; Load '1' from buffer into BL
-
-    cmp rax, rbx
-    jne _finishSpecialFP
-    
-    _firstcontinueEP:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, buffer
-    mov rdx, 2               ; Number of characters from lastPrint to current position
-    syscall
-    
-    mov rsi, [lastPrint]      ; Get the pointer to the last printed position
-    mov rdx, r9               ; Get the current position in text
-    sub rdx, rsi              ; Calculate the length of the remaining text
-    test rdx, rdx             ; Check if there is anything to print
-    jle _finalizeFP             ; Jump to finalize if nothing to print
-
-    ; Print remaining content
-    mov rax, 1                ; syscall number for sys_write
-    mov rdi, 1                ; file descriptor 1 for stdout
-    syscall                   ; Execute the print
-
-_finalizeFP:
-    call _enterPrint
-    call _enterPrint
-    ret
-   
-_finishSpecialFP:
-	mov rsi, [lastPrint]      ; Get the pointer to the last printed position
-    mov rdx, r9               ; Get the current position in text
-    sub rdx, rsi
-    mov qword[lenPrint], rdx
-    
-
-	ret
- 
-;-------------- PRINT DE TODO EL DOCUMENTO CON SUS LINEAS --------------
-
-;--------------------- MANEJO DE EDICION DE CODIGO ---------------------
-_manageEdit:
-	mov byte[flag_printOnePhrase],0
-	mov r9, text_start 
-	call _startFullPrint
-    mov rax, text_escojaLineaEF
-    call _genericprint   
-    call _enterPrint
-    
-    call get_input
-    mov byte[flag_printOnePhrase],1
-    mov r9, text_start 
-    call _startFullPrint
-    call get_inputSPECIAL
-    
-    call _getInputInfo
-    mov byte[flag_printOnePhrase],0
-    call _editText
-    
     ret
 
-_getInputInfo:
-	mov rdi, user_input 
-	call _calculate_size
-	
-	mov rax, qword[lentext]
-	mov qword[lenUserText], rax
-	
-	;---- texto original
-	mov rdi, text_start 
-	call _calculate_size
-	
-	mov rax, qword[lentext]
-	mov qword[lenStartingText], rax
+.next:
+    ; Incrementa el contador
+    inc rcx
+    jmp .loop            
 
-	ret
-	
-get_inputSPECIAL:
-    mov rax, 0         ; syscall number for read
-    mov rdi, 0         ; file descriptor 0 (stdin)
-    mov rsi, user_input; buffer to store the input
-    add rsi, 1         ; Adjust buffer pointer to leave space for the space character
-    mov rdx, 2049      ; reduce max bytes by one to account for the space at the start
-    syscall            ; perform the syscall
+.done:
+    xor rax, rax          ; rax = 0
+    ret
+;---------------COMPARE BINARIO-----------------------------------
 
-    ; Assuming rax contains the number of bytes read, adjust for newline character
-    test rax, rax      ; Check if any bytes were read
-    jz input_done      ; Jump if zero bytes were read (skip if input is empty)
 
-    dec rax            ; Decrement rax to exclude the newline character from the count
-    mov byte [rsi + rax], 0 ; Replace newline with null terminator
-
-    ; Insert space at the beginning of the buffer
-    dec rsi            ; Move back to the start of the buffer
-    mov byte [rsi], ' '; Insert space character
-
-input_done:
-    ret                ; Return from the function
- 
-    
-_editText:
-    ; Copia la primera parte al buffer nuevo
-    mov r8, [lenFirstPart] 
-    sub r8, [lenPrint]
-    
-    mov rsi, text_start
-    mov rdi, new_text
-    mov rcx, r8 ; Termina la primera línea más un espacio
-    rep movsb
-
-    ; Añade text_test
-    mov rsi, user_input
-    mov rcx, [lenUserText]  ; Longitud de 'xd ' sin contar el null terminator
-    rep movsb
-    
-
-	mov r9, [lenStartingText]
-	sub r9, [lenFirstPart]
-	
-	mov rsi, text_start
-    add rsi, [lenFirstPart]  ; Salta la primera y segunda línea
-    mov rcx, r9 ; Longitud total menos lo ya copiado
-    rep movsb
-
-	mov r9, new_text 
-	call _startFullPrint
-	
-    ;mov rax, new_text
-    ;call _genericprint
-
-	ret
 
 
 ;--------------------- MANEJO DE EDICION DE CODIGO ---------------------  
@@ -360,6 +170,99 @@ itoa:
     mov rax, rsi                    ; Devuelve la longitud de la cadena
     ret
 ;-------------------------------- ITOA ---------------------------------
+
+_startItoa2:
+    	;Llama a ITOA para convertir n a cadena
+    	mov rdi, buffer
+    	mov rsi, rsi
+    	mov rbx, 16			;Establece la base (Se puede cambiar)
+    	call itoa2
+    	mov r8, rax  					;Almacena la longitud de la cadena
+    
+    	; Añade un salto de línea
+    	mov byte [buffer + r8], 10
+    	inc r8
+    
+    	; Termina la cadena con null
+		mov byte [buffer + r8], 0
+
+		mov rax, buffer
+		jmp _genericprint
+
+; Definición de la función ITOA
+itoa2:
+    	mov rax, rsi    				; Mueve el número a convertir (en rsi) a rax
+    	mov rsi, 0      				; Inicializa rsi como 0 (contador de posición en la cadena)
+    	mov r10, rbx   					; Usa rbx como la base del número a convertir
+
+.loop2:
+	xor rdx, rdx       					; Limpia rdx para la división
+    	div r10            				; Divide rax por rbx
+    	cmp rbx, 10
+    	jbe .lower_base_digits2 ; Salta si la base es menor o igual a 10
+    
+    	; Maneja bases mayores que 10
+    	movzx rdx, dl
+    	mov dl, byte [digits + rdx]
+    	jmp .store_digit2
+    
+.lower_base_digits2:
+    	; Maneja bases menores o iguales a 10
+    	add dl, '0'    ; Convierte el resto a un carácter ASCII
+    
+.store_digit2:
+    	mov [rdi + rsi], dl  ; Almacena el carácter en el buffer
+    	inc rsi              ; Se mueve a la siguiente posición en el buffer
+    	cmp rax, 0           ; Verifica si el cociente es cero
+    	jg .loop2            ; Si no es cero, continúa el bucle
+    
+    	; Invierte la cadena
+    	mov rdx, rdi
+    	lea rcx, [rdi + rsi - 1]
+    	jmp reversetest2
+    
+reverseloop2:
+		mov al, [rdx]
+    	mov ah, [rcx]
+    	mov [rcx], al
+    	mov [rdx], ah
+    	inc rdx
+    	dec rcx
+    
+reversetest2:
+    	cmp rdx, rcx
+    	jl reverseloop2
+    
+    	mov rax, rsi  ; Devuelve la longitud de la cadena
+    	ret
+
+;------------------------------ATOI-------------------------------------
+; bin_to_hex function
+ascii_to_hex:
+    mov rdi, binary_str
+    mov rsi, hex_result
+    xor r8, r8  ; Clear R8 to use as a loop counter
+.loop:
+    movzx r9, byte [rdi + r8] 
+    test r9, r9 ; Check for null terminator
+    jz .done    
+
+    sub r9, '0' ; Convert ASCII character to its numerical value
+    cmp r9, 9   ; Check if it's a digit
+    jbe .is_digit
+    sub r9, 7   ; Adjust for characters 'A' to 'F'
+.is_digit:
+    ; Convert binary number to hexadecimal
+    mov r11, 0xf ; Mask to isolate lower 4 bits
+    and r11, r9 ; Mask lower 4 bits
+    mov dl, byte [digits + r11] ; Get corresponding hexadecimal digit from lookup table
+    mov [rsi + r8], dl ; Store hexadecimal digit in output string
+    inc r8  ; Increment loop counter
+    jmp .loop   ; Repeat for next character
+.done:
+    ret
+
+;--------------------END ATOI-------------------------------------------
 
 ;------------------------- GENERIC PRINT -------------------------------
 _genericprint:
